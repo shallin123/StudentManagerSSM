47379c59a99267cff1aa99e3efb225bacfcad70f
shallin123 <927002786@qq.com>
2019-09-19 10:52:30 +0800

    2019年9月19日

--- src/main/java/com/shallin/controller/UserController.java
+++ src/main/java/com/shallin/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.shallin.controller;
 
-import com.mysql.cj.util.StringUtils;
-import com.shallin.entity.User;
-import com.shallin.page.Page;
-import com.shallin.service.UserService;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +12,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.shallin.entity.User;
+import com.shallin.page.Page;
+import com.shallin.service.UserService;
 
 /**
- * 管理员控制器
- * @author shallin
+ * 用户（管理员）控制器
+ * @author llq
+ *
  */
 @RequestMapping("/user")
 @Controller
 public class UserController {
+
     @Autowired
     public UserService userService;
 
@@ -30,7 +33,7 @@ public class UserController {
      * @param model
      * @return
      */
-    @RequestMapping(value ="/list",method = RequestMethod.GET)
+    @RequestMapping(value="/list",method=RequestMethod.GET)
     public ModelAndView list(ModelAndView model){
         model.setViewName("user/user_list");
         return model;
@@ -42,59 +45,134 @@ public class UserController {
      * @param page
      * @return
      */
-    @RequestMapping(value = "/get_list",method = RequestMethod.POST)
+    @RequestMapping(value="/get_list",method=RequestMethod.POST)
     @ResponseBody
     public Map<String, Object> getList(
-            @RequestParam(value = "username",required = false,defaultValue ="") String username,
+            @RequestParam(value="username",required=false,defaultValue="") String username,
             Page page
-            ) {
+    ){
         Map<String, Object> ret = new HashMap<String, Object>();
         Map<String, Object> queryMap = new HashMap<String, Object>();
-        queryMap.put("username","%"+username+"%");
-        queryMap.put("offset",page.getOffset());
-        queryMap.put("pageSize",page.getRows());
-        ret.put("rows",userService.findList(queryMap));
-        ret.put("total",10);
+        queryMap.put("username", "%"+username+"%");
+        queryMap.put("offset", page.getOffset());
+        queryMap.put("pageSize", page.getRows());
+        ret.put("rows", userService.findList(queryMap));
+        ret.put("total", userService.getTotal(queryMap));
         return ret;
     }
+
+    /**
+     * 编辑用户操作
+     * @return
+     */
+    @RequestMapping(value="/delete",method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, String> delete(
+            @RequestParam(value="ids[]",required=true) Long[] ids
+    ){
+        Map<String, String> ret = new HashMap<String, String>();
+        if(ids == null){
+            ret.put("type", "error");
+            ret.put("msg", "请选择要删除的数据!");
+            return ret;
+        }
+        String idsString = "";
+        for(Long id:ids){
+            idsString += id + ",";
+        }
+        idsString = idsString.substring(0,idsString.length()-1);
+        if(userService.delete(idsString) <= 0){
+            ret.put("type", "error");
+            ret.put("msg", "删除失败!");
+            return ret;
+        }
+        ret.put("type", "success");
+        ret.put("msg", "修改成功!");
+        return ret;
+    }
+
+    /**
+     * 编辑用户操作
+     * @param user
+     * @return
+     */
+    @RequestMapping(value="/edit",method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, String> edit(User user){
+        Map<String, String> ret = new HashMap<String, String>();
+        if(user == null){
+            ret.put("type", "error");
+            ret.put("msg", "数据绑定出错，请联系开发作者!");
+            return ret;
+        }
+        if(StringUtils.isEmpty(user.getUsername())){
+            ret.put("type", "error");
+            ret.put("msg", "用户名不能为空!");
+            return ret;
+        }
+        if(StringUtils.isEmpty(user.getPassword())){
+            ret.put("type", "error");
+            ret.put("msg", "密码不能为空!");
+            return ret;
+        }
+        User existUser = userService.findByUserName(user.getUsername());
+        if(existUser != null){
+            if(user.getId() != existUser.getId()){
+                ret.put("type", "error");
+                ret.put("msg", "该用户名已经存在!");
+                return ret;
+            }
+
+        }
+        if(userService.edit(user) <= 0){
+            ret.put("type", "error");
+            ret.put("msg", "修改失败!");
+            return ret;
+        }
+        ret.put("type", "success");
+        ret.put("msg", "修改成功!");
+        return ret;
+    }
+
 
     /**
      * 添加用户操作
      * @param user
      * @return
      */
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @RequestMapping(value="/add",method=RequestMethod.POST)
     @ResponseBody
-    public Map<String,String> add(User user){
-        Map<String,String> ret =new HashMap<String, String>();
-        if(user==null){
-            ret.put("type","error");
-            ret.put("msg","数据绑定出错，请联系开发作者");
+    public Map<String, String> add(User user){
+        Map<String, String> ret = new HashMap<String, String>();
+        if(user == null){
+            ret.put("type", "error");
+            ret.put("msg", "数据绑定出错，请联系开发作者!");
             return ret;
         }
-        if(StringUtils.isEmptyOrWhitespaceOnly(user.getUsername())){
-            ret.put("type","error");
-            ret.put("msg","用户名不能为空");
-            return  ret;
+        if(StringUtils.isEmpty(user.getUsername())){
+            ret.put("type", "error");
+            ret.put("msg", "用户名不能为空!");
+            return ret;
         }
-        if(StringUtils.isEmptyOrWhitespaceOnly(user.getPassword())){
-            ret.put("type","error");
-            ret.put("msg","密码不能为空");
-            return  ret;
+        if(StringUtils.isEmpty(user.getPassword())){
+            ret.put("type", "error");
+            ret.put("msg", "密码不能为空!");
+            return ret;
         }
         User existUser = userService.findByUserName(user.getUsername());
-        if(existUser!=null){
-            ret.put("type","error");
-            ret.put("msg","该用户名已经存在");
-            return  ret;
+        if(existUser != null){
+            ret.put("type", "error");
+            ret.put("msg", "该用户名已经存在!");
+            return ret;
         }
-        if (userService.add(user)<=0){
-            ret.put("type","error");
-            ret.put("msg","添加失败");
-            return  ret;
+        if(userService.add(user) <= 0){
+            ret.put("type", "error");
+            ret.put("msg", "添加失败!");
+            return ret;
         }
-        ret.put("type","success");
-        ret.put("msg","添加成功");
-        return  ret;
+        ret.put("type", "success");
+        ret.put("msg", "添加成功!");
+        return ret;
     }
+
 }
